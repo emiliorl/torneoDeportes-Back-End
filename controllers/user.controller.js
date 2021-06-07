662b4ebff97ef2a6ac945e2a0d7f11a3667e757b
Matthew Reyes <mreyes-20161552@kinal.edu.gt>
2021-06-07 17:45:30 -0600

    all user files were createdr

---
+++ controllers/user.controller.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var User = require('../models/user.model');
+var bcrypt = require('bcrypt-nodejs'); 
+var jwt = require('../services/jwt');
+
+var fs = require('fs');
+var path = require('path');
+
+module.exports = {
+    
+}
---
+++ routes/user.routes.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var express = require('express');
+var userController = require('../controllers/user.controller');
+var mdAuth = require('../middleware/authenticated');
+var connectMultiparty = require('connect-multiparty');
+var upload = connectMultiparty({ uploadDir: './uploads/users'});
+
+var api = express.Router();
+
+module.exports = api;
